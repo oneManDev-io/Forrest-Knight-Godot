b94699887eabba942f994eca7bf4c32a678cf8de
Abid Al Wassie <abidalwassie@outlook.com>
2022-12-24 20:53:10 +0600

    added character sprite sheets

--- Prefabs/Player.gd
+++ Prefabs/Player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 @export var moveSpeed : float = 100;
 
-#var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	
@@ -13,15 +12,6 @@ func _physics_process(delta):
 	
 	velocity = inputVector.normalized() * moveSpeed;
 	
-#	var inputVector = Vector2.ZERO;
-#
-#	inputVector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-#	inputVector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-#
-#	if inputVector != Vector2.ZERO:
-#		velocity = inputVector
-#	else:
-#		velocity = Vector2.ZERO
 		
 	move_and_slide()
 	
