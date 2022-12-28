ecafd21b2d9af415f0172b72b023326a371c0573
Abid Al Wassie <abidalwassie@outlook.com>
2022-12-29 00:27:28 +0600

    improved cat ai with random behaviour

--- Prefabs/Cat.gd
+++ Prefabs/Cat.gd
@@ -1,33 +1,55 @@
 extends CharacterBody2D
 
-@export var moveSpeed : float = 20;
+enum catState { Idle, Walk }
 
-@export var facingDirection : Vector2 = Vector2(0, 1)
+@export var moveSpeed: float = 20
+@export var idleTime: float = 1
+@export var walkTime: float = 1
 
-@export var randomVector = Vector2(randi_range(-1,1), randi_range(-1,1))
+#@export var facingDirection= Vector2.ZERO
+#@export var randomVector: Vector2 = Vector2(randi_range(-1,1), randi_range(-1,1))
 
 @onready var animationTree = $AnimationTree
 @onready var stateMachine = animationTree.get("parameters/playback");
+@onready var timer = $Timer
+
+var randomVector: Vector2 = Vector2.ZERO
+var currentState: catState = catState.Idle
 
 func _ready():
-	updateAnimation(facingDirection)
+	pickNewState()
+	updateAnimation(randomVector)
 
 func _physics_process(delta):
+	if(currentState == catState.Walk):
+		velocity = randomVector.normalized() * moveSpeed
+		move_and_slide()
 
-	velocity = randomVector.normalized() * moveSpeed
-
-	updateAnimation(randomVector)
-	move_and_slide()
-	pickNewState()
+func selectNewDirection():
+	randomVector = Vector2(
+		randi_range(-1,1),
+		randi_range(-1,1)
+	)
 
 func updateAnimation(moveInput : Vector2):
-	if(moveInput != Vector2.ZERO):
-		animationTree.set("parameters/Idle/blend_position", moveInput)
+	if(currentState == catState.Walk):
 		animationTree.set("parameters/Walk/blend_position", moveInput)
+		animationTree.set("parameters/Idle/blend_position", moveInput)
 
 func pickNewState():
-	if(velocity != Vector2.ZERO):
+	if(currentState == catState.Idle):
 		stateMachine.travel("Walk")
-	else:
+		currentState = catState.Walk
+		selectNewDirection()
+		updateAnimation(randomVector)
+		timer.start(walkTime)
+	elif(currentState == catState.Walk):
 		stateMachine.travel("Idle");
+		currentState = catState.Idle
+		timer.start(idleTime)
+#		randomVector = Vector2.ZERO
 
+
+
+func _on_timer_timeout():
+	pickNewState()
